b949126c3e82f6816264089b501ffc996a78a6bf
Michael <cherniak.mihail@yandex.ru>
2020-06-01 01:35:06 +0300

    basic api structure created

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"GO-REST-API/internal/app/apiserver"
+	"flag"
+	"github.com/BurntSushi/toml"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	configPath string
+)
+
+func init(){
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml","path to config file")
+}
+
+func main()  {
+	flag.Parse()
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil{
+		logrus.Fatal(err)
+	}
+	s := apiserver.New(config)
+
+	if err := s.Start(); err != nil {
+	logrus.Fatal(err)
+	}
+}
